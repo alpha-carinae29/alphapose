1ec91ef7d9ad2374d1abc6de6b9b76568720efdc
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-12-05 02:10:11 -0800

    add config handler

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+from utils import config_parser
 import argparse
 import torch
 import os
@@ -6,12 +7,17 @@ import cv2
 parser = argparse.ArgumentParser(description="Simplified Demo of AlphaPose for Single Image")
 parser.add_argument("--input_path", type=str, required=True, help="path of the input image")
 parser.add_argument("--output_path", type=str, default="outputs/", help="path for the output result image")
+parser.add_argument("--config_file", type=str, default="configs/config.yaml", help="config file")
 args = parser.parse_args()
+cfg = config_parser.parse(args.config_file)
 
 
 class AlphaPose:
-    def __init__(self, args):
-        pass
+    def __init__(self, args, cfg):
+        self.args = args
+        self.cfg = cfg
+        self.pose_model = builder.build_sppe_model(cfg.MODEL, preset_cfg=cfg.PRESET)
+        self.detection_model = builder.build_detection_model(self.args, self.cfg)
 
     def inference(self, image):
         pass
@@ -22,7 +28,7 @@ class AlphaPose:
 
 def inference():
     output_path = args.output_path
-    model = AlphaPose(args)
+    model = AlphaPose(args, cfg)
     input_path = args.input_path
     image = cv2.imread(input_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
